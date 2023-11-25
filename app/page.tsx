524048b37b454044e33b551272d21704bd5b7c78
ido evergeen <dicksonevergreenido@gmail.com>
2023-11-25 06:25:24 +0100

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Box } from "@chakra-ui/react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
